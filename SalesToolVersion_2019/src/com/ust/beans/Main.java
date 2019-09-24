64ae2bff1b5e3a479f2f496dfd0f01cb9435c342
dev27 <dev27@dev27-desktop>
2019-09-24 14:53:08 +0530

    initial commit by greeshma

---
+++ SalesToolVersion_2019/src/com/ust/beans/Main.java
@@ -0,0 +1,14 @@
+package com.ust.beans;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SalesData salesdata=new SalesData();
+		System.out.println("Welcome to the world");
+		salesdata.display();
+		
+
+	}
+
+}
